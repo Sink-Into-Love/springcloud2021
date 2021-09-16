81f8154fad6a7371efed8311ccee3a3244261895
zph <1305353123@qq.com>
2021-09-16 15:21:23 +0800

    Initial commit

---
+++ src/main/java/com/it/springcloud/Springcloud2021Application.java
@@ -0,0 +1,13 @@
+package com.it.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud2021Application.class, args);
+    }
+
+}
